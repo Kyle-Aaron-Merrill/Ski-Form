566e2db5212507ead728d87864d51385207aa85c
Kyle Merrill <74798513+KyleTehWhale@users.noreply.github.com>
2022-01-09 15:06:09 -0500

    Rebuilt html to reduce clutter and confusion
    Now includes form and fieldset tags. Every input has a unique indetifier.

--- Work Project/type.js
+++ Work Project/type.js
@@ -1,10 +1,17 @@
-function real_bike_hours(){
+function real_bike_hours(that){
     var type_sel = document.getElementById('type_sel');
     var custy_info = document.getElementById('bike_custy_info');
 
+    that.style.color = 'white';
     changeDisplay(type_sel,custy_info);
 }
+function real_ski_hours(that){
+    var type_sel = document.getElementById('type_sel');
+    var custy_info = document.getElementById('ski_custy_info');
 
+    that.style.color = 'white';
+    changeDisplay(type_sel,custy_info);
+}
 function changeDisplay(fade_out_element, fade_in_element){
     var fade_in = fade_in_element; 
     var fade_out = fade_out_element;
@@ -14,7 +21,11 @@ function changeDisplay(fade_out_element, fade_in_element){
     setTimeout(function(){
         fade_out.style.display = 'none';
         fade_in.style.display = 'flex';
-        fade_in.classList.add('fade_in');
     }, 1500);
-    
+    setTimeout(function(){
+        fade_in.classList.add('fade_in');
+    }, 1600);
+}
+function setColor(that){
+    that.style.setColor = 'white';
 }
